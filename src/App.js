7aa1993b72afb9f887da71de5656b1dda64ae694
purusotaman <purusotaman@gmail.com>
2021-04-04 10:47:35 +0530

    Custom form inputs and buttons

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import {  Switch, Route  } from "react-router-dom";
 import HeaderComponent from "./components/header/header.component";
+import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 
 import "./App.css";
-
-const HatsPage = () => (
-  <div className="App">
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 function App() {
   return (
@@ -19,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}  />
         <Route exact path="/shop" component={ShopPage}  />
+        <Route exact path="/signin" component={SignInSignUp}  />
       </Switch>
     </div>
   );
